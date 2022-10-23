b01bd8666c26784262f6a7d864056ce9cdc2af15
xiangbaoyuan <68774115+xiangbaoyuan@users.noreply.github.com>
2022-10-23 12:43:33 +0800

    pull-test

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('push')
+    print_hi('pull')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
